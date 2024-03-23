bfe4bfb6be7ec79256fb7e4592cd20ed29be3c71
TheRexous <144456224+TheRexous@users.noreply.github.com>
2024-03-22 23:17:23 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -11,9 +11,9 @@ for (let i = 0; i < todoList.length; i++) {
 console.log(todoListHTML);
 
 function addTodo() {
-    const inputElement = document.querySelector('js-name-input');
+    const inputElement = document.querySelector('.js-name-input');
     const name = inputElement.value;
-
     todoList.push(name);
     console.log(todoList);
+    inputElement.value = "";
 }
